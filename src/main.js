825974795da22e581fbe3a560bcf9a990e860714
Eriiin <erinnliao@gmail.com>
2024-07-31 00:52:57 +0800

    update code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue' // 匯入App元件
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') // 生成在index.html的<div id="app"></div>當中
